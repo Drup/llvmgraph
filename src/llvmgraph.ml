c178ab68afa01e1f67cd1c8c51261740eaba6fab
Drup <drupyog@zoho.com>
2014-08-11 06:56:36 +0200

    Add the Map functor and improve various functions.

--- src/llvmgraph.ml
+++ src/llvmgraph.ml
@@ -1,6 +1,31 @@
 let id x = x
 
+module Misc = struct
+
+  let is_terminator llv =
+    let open Llvm.ValueKind in
+    let open Llvm.Opcode in
+    match Llvm.classify_value llv with
+      | Instruction (Br | IndirectBr | Invoke | Resume | Ret | Switch | Unreachable)
+        -> true
+      | _ -> false
+
+  let basicblock_in_function llb llf =
+    Llvm.block_parent llb = llf
+
+end
+
+open Misc
+
 module G = struct
+
+  (** Raise Invalid_argument if the basic block is not part of the graph. *)
+  let check_block g b =
+    if basicblock_in_function b g then ()
+    else raise @@
+      Invalid_argument
+        "Llvmgraph: This basic block doesn't belong to this function."
+
 
   type t = Llvm.llvalue
 
@@ -38,18 +63,17 @@ module G = struct
 
   let is_directed = true
 
-
   (** {2 Size functions} *)
 
   let is_empty x = [||] = Llvm.basic_blocks x
   let nb_vertex x = Array.length @@ Llvm.basic_blocks x
+
   let nb_edges _ = failwith "nb_edges: Not implemented"
-
-
 
   (** {2 Successors and predecessors} *)
 
   let succ _g v =
+    check_block _g v ;
     match Llvm.block_terminator v with
       | None -> []
       | Some t ->
@@ -64,11 +88,17 @@ module G = struct
             end
           in aux 0
 
-  let pred _g b =
-    let value = Llvm.value_of_block b in
-    let f l u = (Llvm.instr_parent @@ Llvm.user u) :: l in
-    Llvm.fold_left_uses f [] value
-
+  let pred _g v =
+    check_block _g v ;
+    let llv = Llvm.value_of_block v in
+    let aux l llu =
+      let lli = Llvm.user llu in
+      let llb' = Llvm.instr_parent lli in
+      if is_terminator lli
+      then llb' :: l
+      else l
+    in
+    Llvm.fold_left_uses aux [] llv
 
   let succ_e g v = List.map (fun d -> E.create v () d) @@ succ g v
   let pred_e g v = List.map (fun d -> E.create v () d) @@ pred g v
@@ -82,9 +112,12 @@ module G = struct
 
   (** {2 Membership functions} *)
 
-  let mem_vertex _ _ = true
-  let mem_edge _ _ _ = true
-  let mem_edge_e _ _ = true
+  let mem_vertex g v = basicblock_in_function v g
+  let mem_edge g v1 v2 =
+    basicblock_in_function v1 g &&
+    List.mem v2 @@ succ g v1
+
+  let mem_edge_e g e = mem_edge g (E.src e) (E.dst e)
 
   let find_edge g v1 v2 =
     let dest = List.find (V.equal v2) @@ succ g v1 in
@@ -122,5 +155,32 @@ module G = struct
   let fold_succ_e f g v z = List.fold_left (fun v l -> f l v) z @@ succ_e g v
   let iter_pred_e f g v = List.iter f @@ pred_e g v
   let fold_pred_e f g v z = List.fold_left (fun v l -> f l v) z @@ pred_e g v
+
+end
+
+module Map (B : Graph.Builder.S) = struct
+
+  let map ~vertex ~label g =
+    let h = Hashtbl.create 128 in
+    let f_add_vertex llb g =
+      let v = vertex llb in
+      Hashtbl.add h llb v ;
+      B.add_vertex g v
+    in
+    let f_add_edges llb new_g =
+      let aux e new_g =
+        let lbl = label e in
+        let src = G.E.src e in
+        let dst = G.E.dst e in
+        B.add_edge_e new_g
+          (B.G.E.create (Hashtbl.find h src) lbl (Hashtbl.find h dst))
+      in G.fold_succ_e aux g llb new_g
+    in
+    let new_g =
+      B.empty ()
+      |> G.fold_vertex f_add_vertex g
+      |> G.fold_vertex f_add_edges g
+    in
+    Hashtbl.find h, new_g
 
 end
--- src/llvmgraph.mli
+++ src/llvmgraph.mli
@@ -1,3 +1,12 @@
 
 module G : Graph.Sig.G
   with type V.t = Llvm.llbasicblock
+
+module Map (B : Graph.Builder.S) : sig
+
+  val map :
+    vertex:(G.vertex -> B.G.vertex) ->
+    label:(G.edge -> B.G.E.label) ->
+    G.t -> (G.vertex -> B.G.vertex) * B.G.t
+
+end
