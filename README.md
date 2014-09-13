2accb6d406a7e6ea7732efcebd730740f0e090ff
Drup <drupyog@zoho.com>
2014-09-14 00:16:55 +0200

    Update for the readme and the example.

--- README.md
+++ README.md
@@ -6,3 +6,22 @@ Ocamlgraph overlay for llvm
 The overlay allows you to read and walk (but not write) the control flow graph of an llvm function using the same interface than an ocamlgraph. In particular, read-only ocamlgraph's algorithm can be applied.
 
 It is also possible to use the Map functor and another graph structure to translate an llvm control flow graph to another graph.
+
+All Ocamlgraph functors that work on read-only graph have been pre-applied, to ease usage of the library.
+
+See [the interface](src/llvmgraph.mli) for more details.
+
+## Dependencies ##
+
+- llvm
+- ocamlgraph
+- str for the [colordot test](test/colordot.ml).
+
+## Examples and How-to ##
+
+The [test](test) folder may be consulted to find some interesting uses of this library. In particular, the [colordot example](test/colordot.ml) is annotated with detailed explanations.
+
+Here is the result on [`colordot.c`](test/colordot.c):
+![colordot.dot](http://i.imgur.com/VahGMoP.png)
+
+Other examples are very welcome.
--- test/colordot.ml
+++ test/colordot.ml
@@ -1,6 +1,17 @@
-open Graph
+(* Our goal is to apply the k-coloring algorithm on some llvm control flow graph and to create a dot output correctly colored.*)
+
+(* First, a bit of prelude. *)
 open Llvmgraph
 
+(* A bit of boilerplate to read a bitcode. *)
+let read_bitcode file =
+  let ctx = Llvm.create_context () in
+  let mem = Llvm.MemoryBuffer.of_file file in
+  let m = Llvm_bitreader.parse_bitcode ctx mem in
+  Llvm.MemoryBuffer.dispose mem ;
+  m
+
+(* We will do 3-coloring! Let's define the rgba representations for them. *)
 let to_color h v =
   match Coloring.H.find h v with
     | 0 -> 0x0000FF55l
@@ -9,35 +20,50 @@ let to_color h v =
     | _ -> 0l
 
 let () =
-  let ctx = Llvm.create_context () in
-  let mem = Llvm.MemoryBuffer.of_file Sys.argv.(1) in
-  let m = Llvm_bitreader.parse_bitcode ctx mem in
-  Llvm.MemoryBuffer.dispose mem ;
+  (* Read the bitcode given in the CLI! *)
+  let m = read_bitcode Sys.argv.(1) in
 
+  (* Open the file where we will put the dot output! *)
   let chout = open_out Sys.argv.(2) in
+
+  (* We 3-colors each functions in the bitcode. *)
   Llvm.iter_functions (fun llf ->
+    (* Coloring is a pre-applied functor defined [module Coloring = Graph.Coloring.Make(G)].
+       We use the ocamlgraph implementation directly!
+    *)
     let h = Coloring.coloring llf 3 in
-    let module Dot = Graphviz.Dot (struct
+
+    (* Define a module for dot output using ocamlgraph's Dot functor. *)
+    let module Dot = Graph.Graphviz.Dot (struct
         include G
 
+        (* Boiler-plate for dot definition. *)
         let graph_attributes _ = []
         let default_vertex_attributes _ = []
+        let get_subgraph _ = None
+        let default_edge_attributes _ = []
+        let edge_attributes _ = []
 
+        (* Print the definition of each basic block nicely. *)
         let vertex_name v =
+          (* Fetch a string representation of a basic block. *)
           let s = Llvm.(string_of_llvalue (value_of_block v)) in
+          (* Work around graphviz' crappyness, don't look. *)
           Str.global_replace (Str.regexp "\n") "\\l" (Printf.sprintf "\"%s\"" s)
+        (* Make the output pretty! *)
         let vertex_attributes v = [
           `Fontname "monospace";
           `Shape `Record ;
           `Style `Filled ;
           `FillcolorWithTransparency (to_color h v) ;
         ]
-        let get_subgraph _ = None
-        let default_edge_attributes _ = []
-        let edge_attributes _ = []
 
       end)
     in
+
+    (* Output the dot. *)
     Dot.output_graph chout llf
   ) m ;
+
+  (* Clean up the mess before leaving. *)
   close_out chout
